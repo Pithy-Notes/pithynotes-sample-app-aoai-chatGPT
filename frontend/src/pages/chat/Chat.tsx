5322550f8b2fa5a8ee8ac3f0ccc632b943d06e44
pithynotes <154715881+pithynotes@users.noreply.github.com>
2024-01-11 22:18:53 -0800

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -9,7 +9,6 @@ import uuid from 'react-uuid';
 import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
-import pithynotes from "../../assets/pithynotes.svg";
 
 import {
     ChatMessage,
@@ -585,11 +584,7 @@ const Chat = () => {
                     <div className={styles.chatContainer}>
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
-                                <img
-                                    src={pithynotes}
-                                    className={styles.chatIcon}
-                                    aria-hidden="true"
-                                />
+                               
                                 <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
                                 <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
                             </Stack>
